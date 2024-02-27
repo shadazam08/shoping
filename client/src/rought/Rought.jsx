06ed8238423b3a8898e3b7b9ebc31c976aaec388
Shad Azam <azamshad08@gmail.com>
2024-02-27 15:52:33 +0530

    update by Azam

--- client/src/rought/Rought.jsx
+++ client/src/rought/Rought.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import UserAuth from '../pages/userAuth/UserAuth'
+// import UserAuth from '../pages/userAuth/UserAuth'
+import HomePage from '../pages/HomePage/HomePage'
 
 const Rought = () => {
     return (
         <>
-            <UserAuth />
+            <HomePage />
         </>
     )
 }
