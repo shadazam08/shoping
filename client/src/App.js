b4cc5dba6f42cd2b71bb2ec099bf8ccf33a41014
Shad Azam <azamshad08@gmail.com>
2024-02-27 14:21:14 +0530

    update by Azam

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Shad Azam
+      </header>
+    </div>
+  );
+}
+
+export default App;
